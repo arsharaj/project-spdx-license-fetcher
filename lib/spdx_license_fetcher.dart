0ee25efb867f0aa5cdc2c3a9c8854b521a04693c
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2024-06-24 00:28:43 +0530

    feat : created the library for spdx license fetcher

---
+++ lib/spdx_license_fetcher.dart
@@ -0,0 +1,7 @@
+library spdx_license_fetcher;
+
+export 'src/exceptions.dart' show DirectoryNotFoundException, FileNotValidException;
+export 'src/file_scanner.dart' show FileScanner;
+export 'src/license_identification_service.dart' show LicenseIdentificationService, LicenseIdentificationServiceImpl;
+export 'src/license_identifier.dart' show TextFileLicenseIdentifier;
+export 'src/report_generator.dart' show ReportGenerator;
